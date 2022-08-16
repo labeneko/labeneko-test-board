63ee2733cd2062906047a15e3468052331ad9544
labeneko <labecat@gmail.com>
2022-08-16 19:16:08 +0900

    パス変更

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { app } from '@lib/firebase';
+import { app } from '../lib/firebase';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
